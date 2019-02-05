b45bf20a0da6cce2a0f49917fc3c7fd383104b69
noragne <noragne@gmail.com>
2019-02-05 22:20:49 +0100

    Updates

---
+++ precache-manifest.dba1aabe05941697e9ae0291244fb5ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b245c8ed332a53bf0b08",
+    "url": "/hackernews/static/css/main.3a915d34.chunk.css"
+  },
+  {
+    "revision": "b245c8ed332a53bf0b08",
+    "url": "/hackernews/static/js/main.b245c8ed.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/hackernews/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "da1602332b801076df13",
+    "url": "/hackernews/static/js/runtime~main.da160233.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hackernews/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ce73aa3670f2e1e2b5c151a298295529",
+    "url": "/hackernews/index.html"
+  }
+];
